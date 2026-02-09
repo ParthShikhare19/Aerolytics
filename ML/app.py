e52a17c6731398292c44c3bd3e3ef44c0e5e859a
ParthShikhare19 <parthshikhare21@gmail.com>
2026-02-09 20:56:33 +0530

    styling of prefiction form changed

--- ML/app.py
+++ ML/app.py
@@ -7,6 +7,7 @@ f.close()
 
 
 app= Flask(__name__)
+app.secret_key = 'your-secret-key-here'
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -18,9 +19,11 @@ def home():
         bmi = float(request.form.get("Humidity"))
         dpf = float(request.form.get("Gas Resistance"))
 
-        msg = model.predict([[glucose, bp, skin, insulin, bmi, dpf]])
-
-        return render_template("home.html", msg=msg)
+        prediction = model.predict([[glucose, bp, skin, insulin, bmi, dpf]])
+        msg = f"Predicted AQI: {int(round(prediction[0]))}"
+        
+        flash(msg, 'success')
+        return redirect(url_for('home'))
 
     return render_template("home.html")
 
